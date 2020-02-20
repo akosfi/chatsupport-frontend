2ab02a02300740936db1bca0bef66a47cfe0e0b3
akosfi <fiakos1997@gmail.com>
2020-02-20 15:32:25 +0100

    added state management

---
+++ src/store/modules/socket.js
@@ -0,0 +1,24 @@
+const state = {
+    connected: false,
+    address: "http://localhost:3000",
+};
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations,
+}
